adb7d21034005b56874d59c498aa06a36799dcf3
Deepak Surya <deepakool02@gmail.com>
2020-08-22 19:42:50 +0530

    albums working

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var Upload=require('./models/upload');
 var Album=require('./models/album');
 
 var userId;
+var albumId;
 var upload=require('express-fileupload');
 var Jimp = require('jimp');
 const path = require('path');
@@ -45,7 +46,7 @@ passport.deserializeUser(User.deserializeUser());
 app.get('/', function(req, res){
  
     Upload.find(function(err,uploadDB){
-        console.log(uploadDB);
+        //console.log(uploadDB);
         res.render('dashboard',{uploads:uploadDB});
 
     })
@@ -53,7 +54,7 @@ app.get('/', function(req, res){
  });
 
 app.get('/login',function(req,res){
-    console.log(req.user);
+    //console.log(req.user);
 
     res.render('login');
 
@@ -64,7 +65,7 @@ app.post('/login',passport.authenticate('local'),function(req,res){
 
     User.findOne({username:req.body.username},function(err,found){
         if(err){
-            console.log(err);
+            //console.log(err);
         }else{
             res.redirect('/home/'+found._id);
         }
@@ -76,7 +77,7 @@ app.post('/login',passport.authenticate('local'),function(req,res){
 app.get('/logout', function(req, res){
                 req.logout();
                res.redirect('/');
-               console.log(req.user);
+               //console.log(req.user);
              });
             
 
@@ -89,7 +90,7 @@ app.post('/register',function(req,res){
  
      User.register(new User({username:req.body.username}),req.body.password,function(err,newCreatedUser){
          if(err){
-             console.log(err);
+             //console.log(err);
              return res.render('register');
          }else{
  
@@ -99,7 +100,7 @@ app.post('/register',function(req,res){
              newCreatedUser.description=null;
              newCreatedUser.position=null;
 
-             console.log(newCreatedUser);
+             //console.log(newCreatedUser);
  
              passport.authenticate("local")(req,res,function(){
                  res.redirect('/login');
@@ -117,7 +118,7 @@ app.post('/register',function(req,res){
     if(req.user){
         User.findById(req.params.id,function(err,found){
             if(err){
-                console.log(err);
+                //console.log(err);
             }else{
                 res.render('home',{user:found});
                 userId=req.params.id;
@@ -142,7 +143,7 @@ app.get('/home/:id/edit',function(req,res){
     if(req.user){
         User.findById(req.params.id,function(err,found){
             res.render('info',{user:found});
-            //console.log(found);
+            ////console.log(found);
             });
             
     }else{
@@ -161,7 +162,7 @@ app.post('/home/:id/edit',function(req,res){
 
         User.findById(userId,function(err,found){
             if(err){
-                console.log(err);
+                //console.log(err);
             }else{
             found.username=req.body.username;
             found.email=req.body.email;
@@ -223,13 +224,12 @@ app.post('/home/:id/gallery',function(req,res){
 
     if(req.user){
 
-            if(req.files){
-                console.log(req.files);
+                //console.log(req.files);
                 
                 var file=req.files.filename;
                 var filename=file.name;
                 fileName=filename;
-                console.log(filename);
+                //console.log(filename);
                 var fileSaveLocationOrginal='/upload/'+filename;
                 file.mv('./public/upload/'+filename,function(err,suc){
                     if(err){console.log(err)}else{
@@ -253,7 +253,7 @@ app.post('/home/:id/gallery',function(req,res){
                     //foundUser.modifiedUploads.push(fileSaveLocationModified);
                     //foundUser.save();
                     if(err){
-                        console.log(err);
+                        console.log(err,'256');
                     }
                     Upload.create({
     
@@ -263,9 +263,9 @@ app.post('/home/:id/gallery',function(req,res){
     
                     },function(err,created){
                         if(err){
-                            console.log(err);
+                            console.log(err,'266');
                         }else{
-                            console.log(created);
+                            //console.log(created);
                             created.save();
                             foundUser.uploads.push(created);
                             foundUser.save();
@@ -283,15 +283,8 @@ app.post('/home/:id/gallery',function(req,res){
         
         
             }else{
-                console.log('no');
+                //console.log('no');
             }
-    
-    
-    }else{
-        res.redirect('/login')
-    }
-
-    
     
 });
 
@@ -316,6 +309,7 @@ app.get('/home/:id/gallery/:uploadId',function(req,res){
 
     
     User.findById(req.params.id).populate('albums').exec(function(err,foundUser){
+
         res.render('album',{user:foundUser});
 
     });
@@ -346,8 +340,103 @@ app.post('/home/:id/myalbums',function(req,res){
 
 
 
+app.get('/home/:id/myalbums/:album',function(req,res){
+    albumId=req.params.album;
+    Album.findById(req.params.album).populate('gallery').exec(function(err,found){
+        res.render('albumgallery',{album:found,user:userId});
+
+    })
+});
+
+app.get('/home/:id/myalbums/:album/addtoalbum',function(req,res){
+
+    User.findById(req.params.id).populate('albums').exec(function(err,foundUser){
+
+        res.render('addtoalbum',{user:foundUser,albumId:albumId});
+
+    });
+});
+
+app.post('/home/:id/myalbums/:album',function(req,res){
+   
+
+        if(req.files){
+            //console.log(req.files);
+            
+            var file=req.files.filename;
+            var filename=file.name;
+            fileName=filename;
+            console.log(filename);
+            var fileSaveLocationOrginal='/upload/'+filename;
+            file.mv('./public/upload/'+filename,function(err,suc){
+                if(err){console.log(err)}else{
+                    console.log('dome');
+                }
+            });
+    
+            var fileSaveLocationModified='/new/'+filename;
+    
+            Jimp.read('./public/upload/'+filename, (err, lenna) => {
+                if (err) throw err;
+                lenna
+                  .resize(150, 112) // resize
+                  .quality(40) // set JPEG quality
+                  //.greyscale() // set greyscale
+                  .write('./public/new/'+filename); // save
+              });
+    
+              User.findById(userId,function(err,foundUser){
+ 
+                if(err){
+                    console.log(err);
+                }else{
+                    Upload.create({
+
+                        username:foundUser.username,
+                        orginalUpload:fileSaveLocationOrginal,
+                        modifiedUpload:fileSaveLocationModified
+    
+                    },function(err,created){
+                        if(err){
+                            console.log(err);
+                        }else{
+                            console.log(created);
+                            created.save();
+                            foundUser.uploads.push(created);
+                            foundUser.save();
+                            
+                            Album.findById(albumId,function(err,albumFound){
+                               albumFound.gallery.push(created);
+                                albumFound.save();
+                               //console.log("albumFound",albumFound,'created',created);
+                            });
+                            
+                            
+                            
+                    
+    
+                            
+    
+                        }
+                    });
+                }
+
+               
+
+            });
 
 
+    
+            
+    
+    
+    
+        }else{
+            console.log('no');
+        }
+
+   
+});
 
 
 app.listen(1010,function(){
